cc0ddd448ef29e04bf115a01f05ab6b3de640d60
bernst4315 <bernst4315@gmail.com>
2025-01-08 13:34:14 -0700

    finished part 2

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-const adventurer = {
-  name: "Robin",
-  health: 10,
-  inventory: ["sword", "potion", "artifact"],
-  companion: {
-    name: "Leo",
-    type: "Cat",
-    subCompanion:{
-        name: "Frank",
-        type: "Flea",
-        inventory: ["small hat", "sunglasses"]
-    }
-  },
-  roll (mod = 0) {
-    const result = Math.floor(Math.random() * 20) + 1 + mod;
-    console.log(`${this.name} rolled a ${result}.`)
-    }
-};
+//Part 1
 
-adventurer.roll()
+// const adventurer = {
+//   name: "Robin",
+//   health: 10,
+//   inventory: ["sword", "potion", "artifact"],
+//   companion: {
+//     name: "Leo",
+//     type: "Cat",
+//     subCompanion:{
+//         name: "Frank",
+//         type: "Flea",
+//         inventory: ["small hat", "sunglasses"]
+//     }
+//   },
+//   roll (mod = 0) {
+//     const result = Math.floor(Math.random() * 20) + 1 + mod;
+//     console.log(`${this.name} rolled a ${result}.`)
+//     }
+// };
+
+// adventurer.roll()
+
+//Part 2
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+   
+    }
+
+       roll(mod = 0){
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
+  }
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.roll()
+robin.companion.roll()
+robin.companion.companion.roll()
