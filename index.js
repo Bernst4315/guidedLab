067a3b9e34fb3b70320df077d5df734e36907ff3
bernst4315 <bernst4315@gmail.com>
2025-01-08 14:14:22 -0700

    part 4 done

--- index.js
+++ index.js
@@ -51,7 +51,57 @@
 
 //part 3
 
-class Character {
+// class Character {
+//     constructor (name) {
+//       this.name = name;
+//       this.health = 100;
+//       this.inventory = [];
+//     }
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//         }
+//   }
+
+//   class Adventurer extends Character {
+//     constructor (name, role) {
+//       super(name);
+//       // Adventurers have specialized roles.
+//       this.role = role;
+//       // Every adventurer starts with a bed and 50 gold coins.
+//       this.inventory.push("bedroll", "50 gold coins");
+//     }
+//     // Adventurers have the ability to scout ahead of them.
+//     scout () {
+//       console.log(`${this.name} is scouting ahead...`);
+//       super.roll();
+//     }
+//   }
+
+//   class Companion extends Character {
+//     constructor(name, type){
+//         super(name);
+//         this.type = type; 
+//     }
+//     run(){
+//         console.log(`${this.name} is running away!`)
+//         super.roll()
+//     }
+//   }
+
+//   const robin = new Adventurer("Robin", "Adventurer");
+//   const frank = new Companion("Frank", "Companion");
+//   const leo = new Companion("Leo", "Companion"); 
+
+//   robin.scout()
+//   frank.run()
+//   leo.run()
+
+  //part4
+
+  class Character {
+    static Max_Health = 100; 
+    
     constructor (name) {
       this.name = name;
       this.health = 100;
@@ -61,17 +111,17 @@ class Character {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
         }
+    
   }
 
   class Adventurer extends Character {
+    static roles = ["Fighter", "Healer", "Mage"]
     constructor (name, role) {
       super(name);
-      // Adventurers have specialized roles.
       this.role = role;
-      // Every adventurer starts with a bed and 50 gold coins.
       this.inventory.push("bedroll", "50 gold coins");
     }
-    // Adventurers have the ability to scout ahead of them.
+   
     scout () {
       console.log(`${this.name} is scouting ahead...`);
       super.roll();
@@ -96,3 +146,6 @@ class Character {
   robin.scout()
   frank.run()
   leo.run()
+
+  console.log(Adventurer.Max_Health)
+  console.log(Adventurer.roles[0])
